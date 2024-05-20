abf15c5f087382a345484e944a461cea3e45cbaf
nicolasque <quequecedo@gmail.com>
2024-05-20 17:01:12 +0200

    ty to get in macbook new changes

--- main.c
+++ main.c
@@ -6,51 +6,14 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 20:07:40 by nquecedo          #+#    #+#             */
-/*   Updated: 2024/05/17 18:06:27 by nquecedo         ###   ########.fr       */
+/*   Updated: 2024/05/17 18:23:41 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "push_swap.h"
 
-//If there is a leter in the args rturns 1
-int ft_is_all_num(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '-' || str[i] == '+')
-			i++;
-		if (!ft_isdigit(str[i]))
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int ft_is_repeat(int argc, char** argv, char *str, int list_position)
-{
-	int	i;
-
-	i = 1;
-	while (i < argc)
-	{
-		if (i == list_position)
-			return (0);
-		else
-		{
-			if (!ft_strncmp(str, argv[i], 11))
-				return (-1);
-		}
-		i ++;
-	}
-	return (0);
-}
-
-
-int	ft_check_args(int argc, char **argv)
+int	ft_init_stack(int argc, char **argv, t_stak *a)
 {
 	int i;
 
