8f875942fa0e7ff19e9c4ff4ef2b8ebc2b9b41d7
nicolasque <37843574+nicolasque@users.noreply.github.com>
2024-06-27 09:45:43 +0000

    refactor: Update ft_ez_short function for better code organization and readability

--- ez_short.c
+++ ez_short.c
@@ -6,77 +6,76 @@
 /*   By: nquecedo <nquecedo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 08:08:07 by nquecedo          #+#    #+#             */
-/*   Updated: 2024/06/20 11:49:37 by nquecedo         ###   ########.fr       */
+/*   Updated: 2024/06/27 09:45:38 by nquecedo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int  ft_ez_short(t_stack **a, t_stack **b)
+int	ft_ez_short(t_stack **a, t_stack **b)
 {
-    int list_len;
+	int	list_len;
 
-    list_len = ft_list_len(*a);
-    if (list_len > 4)
-        return (0);
-    if (list_len == 2)
-        ft_short_two(a);
-    else if (list_len == 3)
-        ft_short_three(a);
-    else if (list_len == 4)
-        ft_short_four(a, b);
-    return (1);
+	list_len = ft_list_len(*a);
+	if (list_len > 4)
+		return (0);
+	if (list_len == 2)
+		ft_short_two(a);
+	else if (list_len == 3)
+		ft_short_three(a);
+	else if (list_len == 4)
+		ft_short_four(a, b);
+	return (1);
 }
 
-void    ft_short_two(t_stack **a)
+void	ft_short_two(t_stack **a)
 {
-    ft_sa(a);
-    ft_free_stack(*a);
-    exit(0);
+	ft_sa(a);
+	ft_free_stack(*a);
+	exit(0);
 }
 
-void ft_short_three(t_stack **a)
+void	ft_short_three(t_stack **a)
 {
-    if ((*a)->value > (*a)->next->value)
-    {
-        if ((*a)->next->value > (*a)->next->next->value) 
-        {
-            ft_sa(a);
-            ft_rra(a);
-        } 
-        else if ((*a)->value > (*a)->next->next->value) 
-            ft_ra(a);
-        else
-        {
-            ft_rra(a);
-            ft_sa(a);
-            ft_rra(a);
-        }
-        return ;
-    }
-    if ((*a)->value > (*a)->next->next->value)
-        ft_rra(a);
-    else if ((*a)->next->value > (*a)->next->next->value)
-    {
-        ft_sa(a);
-        ft_ra(a);
-    }
+	if ((*a)->value > (*a)->next->value)
+	{
+		if ((*a)->next->value > (*a)->next->next->value)
+		{
+			ft_sa(a);
+			ft_rra(a);
+		}
+		else if ((*a)->value > (*a)->next->next->value)
+			ft_ra(a);
+		else
+		{
+			ft_rra(a);
+			ft_sa(a);
+			ft_rra(a);
+		}
+		return ;
+	}
+	if ((*a)->value > (*a)->next->next->value)
+		ft_rra(a);
+	else if ((*a)->next->value > (*a)->next->next->value)
+	{
+		ft_sa(a);
+		ft_ra(a);
+	}
 }
 
-void    ft_short_four(t_stack **a, t_stack **b)
+void	ft_short_four(t_stack **a, t_stack **b)
 {
-    t_stack *min_node;
+	t_stack	*min_node;
 
-    min_node = ft_get_min_node(*a);
-    
-    while ((*a)->value != min_node->value)
-        if ((*a)->index <= 2)
-            ft_ra(a);
-        else
-            ft_rra(a);
-    ft_pb(a, b);
-    ft_short_three(a);
-    ft_pa(a, b);
+	min_node = ft_get_min_node(*a);
+	while ((*a)->value != min_node->value)
+	{
+		if ((*a)->index <= 2)
+			ft_ra(a);
+		else
+			ft_rra(a);
+	}
+	ft_pb(a, b);
+	ft_short_three(a);
+	ft_pa(a, b);
 }
-
-
